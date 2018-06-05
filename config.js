b19796d4921ce05706a108f7e0eeac8518e83449
RrhythmSharma <rrhythmsharma@gmail.com>
2018-06-05 12:08:39 +0530

    Updated config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    FB_PAGE_TOKEN: 'EAAYX635f2TEBABS59AkqM3AHepddfTN3LqKnFXh4r448gbZBCXJ3ft5JQzS8d74M8LidLNDX4sse4RGOVNyibqsiudfiZBc1JtT5FzS5O9Fm3mXviWa1aZAzt2ZAc8WJBrrqng87Hvrw3ZBicbYGMlrF69ZAtjoiQeg0SfEhyBaa3nGDhqTyJVWRUqcytZCKRkZD',
-    FB_VERIFY_TOKEN: 'jiojiojio',
-    API_AI_CLIENT_ACCESS_TOKEN: '946f0694a746413c9c60363e3951c769',
-    FB_APP_SECRET: 'ff0365242d8c2285362460350a2125d8',
-    SERVER_URL: " https://smartbot-api.herokuapp.com/",
-    SENDGRID_API_KEY: 'Your key'
-    EMAIL_TO:''
-    EMAIL_FROM:''
+    FB_PAGE_TOKEN: '',//enter your facebook page token
+    FB_VERIFY_TOKEN: '',//enter your verifcation
+    API_AI_CLIENT_ACCESS_TOKEN: '',//you can find API_AI_CLIENT_ACCESS_TOKEN iN DIALOGFLOW
+    FB_APP_SECRET: '',//you can find FB_APP_SECRET from facebook app
+    SERVER_URL: "",//your server url , you can find url in your heroku app
+    SENDGRID_API_KEY: ''//generate this key from heroku
+    EMAIL_TO:''//your testing Email
+    EMAIL_FROM:''//your testing Email
 };
